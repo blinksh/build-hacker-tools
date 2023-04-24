f09e81cd7711b935b52f25bcf6171dadab937868
Yury Korolev <yury.korolev@gmail.com>
2023-04-24 13:23:36 +0300

    Fix neovim

--- library-scripts/nvim-debian.sh
+++ library-scripts/nvim-debian.sh
@@ -5,12 +5,17 @@ echo "Installing NeoVim"
 set -eux;
 dpkgArch="$(dpkg --print-architecture)";
 case "${dpkgArch##*-}" in
-    amd64) dep='linux64.deb' ;;
+    amd64) dep='linux64.tar.gz' ;;
     *) echo >&2 "unsupported architecture: ${dpkgArch}"; exit 1 ;;
 esac;
 
 url="https://api.github.com/repos/neovim/neovim/releases/latest"
-curl -L -o nvim-linux64.deb `curl -s "$url" | jq -r '.assets[].browser_download_url' | grep "$dep"`
-apt install ./nvim-linux64.deb
+curl -L -o nvim-linux64.tar.gz `curl -s "$url" | jq -r '.assets[].browser_download_url' | grep "$dep"`
+mkdir nvim
+tar -zxf nvim-linux64.tar.gz -C nvim --strip-components=1
 
-rm -rf nvim-linux64.dep
+mv nvim/bin/* /usr/local/bin/
+mv nvim/lib/* /usr/local/lib/
+cp -r nvim/share/* /usr/local/share/
+
+rm -rf nvim
