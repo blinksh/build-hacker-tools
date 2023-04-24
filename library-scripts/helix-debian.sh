f2381b4d3b57ded263ac7c4e248ed087b019dc80
Yury Korolev <yury.korolev@gmail.com>
2023-04-24 10:37:16 +0300

    Try to fix hellix install

--- library-scripts/helix-debian.sh
+++ library-scripts/helix-debian.sh
@@ -3,26 +3,30 @@ export HELIX_RUNTIME=$HELIX/runtime
 
 echo "Installing Helix"
 
-mkdir -p $HELIX
+#mkdir -p $HELIX
 
-set -eux;
-dpkgArch="$(dpkg --print-architecture)";
-case "${dpkgArch##*-}" in
-    amd64) rustArch='x86_64-linux' ;;
-    *) echo >&2 "unsupported architecture: ${dpkgArch}"; exit 1 ;;
-esac;
+add-apt-repository ppa:maveonair/helix-editor
+apt update
+apt install helix
 
-url="https://api.github.com/repos/helix-editor/helix/releases/latest"
-curl -L -o helix.tar.xz `curl -s "$url" | jq -r '.assets[].browser_download_url' | grep "$rustArch"`
-mkdir helix
-tar -xf helix.tar.xz -C helix --strip-components=1
+# set -eux;
+# dpkgArch="$(dpkg --print-architecture)";
+# case "${dpkgArch##*-}" in
+#     amd64) rustArch='x86_64-linux' ;;
+#     *) echo >&2 "unsupported architecture: ${dpkgArch}"; exit 1 ;;
+# esac;
 
-mv helix/hx /usr/local/bin/
-mv helix/runtime $HELIX
+# url="https://api.github.com/repos/helix-editor/helix/releases/latest"
+# curl -L -o helix.tar.xz `curl -s "$url" | jq -r '.assets[].browser_download_url' | grep "$rustArch"`
+# mkdir helix
+# tar -xf helix.tar.xz -C helix --strip-components=1
 
-rm -rf helix
+# mv helix/hx /usr/local/bin/
+# mv helix/runtime $HELIX
 
-hx --grammar fetch
-hx --grammar build
+# rm -rf helix
 
-echo 'export HELIX_RUNTIME=/usr/local/.config/helix/runtime' > /etc/profile.d/helix.sh
+# hx --grammar fetch
+# hx --grammar build
+
+# echo 'export HELIX_RUNTIME=/usr/local/.config/helix/runtime' > /etc/profile.d/helix.sh
